6c2dfc7291a95e6f432514418f904a073014ee02
Gleb Batyan <66956508+Batlay@users.noreply.github.com>
2025-03-05 10:45:37 +0300

    add auth provider

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,8 +1,19 @@
-function UserIcon() {
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { LuUser } from "react-icons/lu";
+
+
+
+async function UserIcon() {
+  const user = await currentUser()
+
+  const profileImage = user?.imageUrl
+
+  if (profileImage) {
+    return <img src={profileImage} className="w-6 h-6 rounded-full object-cover"/>
+  }
+
   return ( 
-    <div>
-      Cart
-    </div>
+    <LuUser className='w-6 h-6 bg-primary rounded-full text-white' />
   );
 }
 
